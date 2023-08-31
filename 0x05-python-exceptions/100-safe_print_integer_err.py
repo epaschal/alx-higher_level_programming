7ec310776460a069868233e41b477e85c312311a
epaschal <paschalejioguody@gmail.com>
2023-08-31 09:47:40 -0700

    100-safe_print_integer_err.py

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    import sys
+
+    try:
+        print("{:d}".format(value))
+    except ValueError as err:
+        sys.stderr.write("Exception: {}\n".format(err))
+        return False
+    else:
+        return True
