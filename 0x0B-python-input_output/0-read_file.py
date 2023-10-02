3d964bb3ba7cbdb412582e07a3c202c59b4755ff
epaschal <paschalejioguody@gmail.com>
2023-10-02 14:56:50 -0700

    input output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-""" Module that contains a function that reads a text file
-"""
+""" Module that contains a function that reads from a file """
+
 
 def read_file(filename=""):
-    """ Function that reads a text file
+    """ Function that reads from a file
 
     Args:
         filename: filename
 
-    Raises:
-        Exception: if file not found
+    Raises
+        Exception: when the file can be opened
+
     """
+
     with open(filename, 'r', encoding="utf-8") as f:
-        read_file = f.read()
-        print(read_file, end="")
+        read_data = f.read()
+        print(read_data, end='')
